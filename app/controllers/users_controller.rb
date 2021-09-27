c23c75fac670c9c423dabeccef44fe3b731df4bc
manishmowall <manishmowall@gmail.com>
2021-09-28 01:51:23 +0530

    url restrictons implimented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]  
+  before_action :require_same_user, only: [:edit, :update, :destroy]  
+
     def show
         
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -42,6 +45,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to articles_path 
+    end
+
 
     private
 
@@ -51,5 +61,11 @@ class UsersController < ApplicationController
 
         def set_user
             @user = User.find(params[:id])
+        end
+        def require_same_user
+            if current_user != @user
+                flash[:alert] = "You can only edit or delete your own article"
+                redirect_to @user
+            end
         end
 end 
